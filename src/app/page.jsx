2bde73bba973a947b66be9e7632fe6479bed764c
Prathameshzad <prathameshzad20@gmail.com>
2025-06-12 22:26:27 +0530

    Update the Landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,23 @@
+import HeroSection from "@/components/landingPage/HeroSection";
+import Navbar from "../components/landingPage/Navbar";
+import { ContainerScroll } from "@/components/landingPage/ui/container-scroll-animation";
+import Team from "@/components/landingPage/Team";
+import Members from "@/components/landingPage/Members";
+import Card from "@/components/landingPage/Card";
+import Footer from "@/components/landingPage/Footer";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-100 py-10">
-      hello
+    <main>
+      <div>
+        <Navbar/>
+        <HeroSection/>
+        <ContainerScroll/>
+        <Team/>
+        {/* <Members/> */}
+        <Card/>
+        <Footer/>
+      </div>
     </main>
   );
 }
